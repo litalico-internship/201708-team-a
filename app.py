2d89a484c36785ab176a9691e8421b080a3b4295
wattan0 <sasakiwataru@sasakiwataru-no-MacBook-Pro.local>
2017-08-19 17:29:01 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import flask
+from flask import render_template
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
